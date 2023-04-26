e054c3fab1abd9917da388366b084791e93a245f
isaac vega <ivega@analyze.cl>
2023-04-26 12:01:09 -0400

    fix se agrega animaciones keyframes

--- src/components/StyledComponentExplicacion/Styled.js
+++ src/components/StyledComponentExplicacion/Styled.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 
 const P = styled.p`
     font-size:24px;
@@ -10,15 +10,32 @@ const Content = styled.div`
 `
 
 const Button = styled.button`
+    transition: box-shadow 0.2s ease;
     background-color: ${props => props.primary ? 'red' : 'white'};
     color: ${props => props.primary ? 'white' : 'red'};
     padding: 10px 15px;
+    margin: 5px;
     border: solid 2px red;
     border-radius: 4px;
     cursor: pointer;
+
+    &:hover{
+        box-shadow: 5px 2px 5px rgb(50,0,50,0.3);
+        border: solid 2px #fff;
+    }
+
+    &.secundary{
+        background-color: blue;
+        border: solid 2px #fff;
+    }
+
+    .info{
+        font-size: 28px;
+    }
 `
 
 const BlockButton=styled(Button)`
+    margin: 10px 0;
     width:100%;
     font-size:24px;
 `
@@ -28,26 +45,67 @@ const Link = ({ className, ...props}) => {
 }
 
 const StyledLink=styled(Link)`
+    margin: 10px;
     color:blue;
 `
 
+// const Input = () => {
+//     return(
+//         <input type="text" className={className}/>
+//     )
+// }
+
+const Input = styled.input.attrs(props => ({
+    type: 'text',
+    color: props.color || 'red'
+}))`
+    font-size: 20px;
+    border: 1px solid red;
+    //puedo interseptar las propiedades
+    color: ${props => props.color}
+`
+
+const Password = styled(Input).attrs({
+    type: 'password'
+})``
+
+
+const girar = keyframes`
+    from {
+        transform: rotate(0deg);
+    }
+
+    to {
+        transform: rotate(360deg);
+    }
+`
+
+const Rotar = styled.div`
+    animation: ${girar} 2s linear infinite;
+    display: inline-block;
+`
 const Styled = () => {
- return(
-    <Content>
-        <h1>
-            Styled Components
-        </h1>
-        <P>Hola soy un parrafo</P>
-        <Button>Enviar</Button>
-        <Button primary>Enviar</Button>
-        <BlockButton>Enviar</BlockButton>
-        <BlockButton primary as='a' href='#'>Enviar</BlockButton>
-        <Link>LINK</Link>
-        <StyledLink>LINK STYLED</StyledLink>
+    return(
+        <Content>
+            <h1>
+                Styled Components
+            </h1>
+            <P>Hola soy un parrafo</P>
+            <Button>Enviar</Button>
+            <Button>Enviar<p className="info">info</p></Button>
+            <Button primary>Enviar</Button>
+            <Button primary className="secundary">Enviar</Button>
+            <BlockButton>Enviar</BlockButton>
+            <BlockButton primary as='a' href='#'>Enviar</BlockButton>
+            <Link>LINK</Link>
+            <StyledLink>LINK STYLED</StyledLink>
+            <Input color='blue'/>
+            <Password/>
+            <br/>
+            <Rotar>Estoy Girando</Rotar>
+        </Content>
 
-    </Content>
-
- )
+    )
 }
 
 export default Styled
